1a3804e250a188ee149aa5458716e4dcc4c653ba
surajkumar731974-rgb <surajkumar731974@gmail.com>
2026-03-23 05:03:47 +0530

    Remove /update route (use Not Found: Update Soon)

--- app/(main)/update/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Update() {
-  return(
-      <div className="flex items-center justify-center h-screen">
-        <h2 className="text-4xl font-bold text-[#0b3c5d] [text-shadow:2px_2px_4px_rgba(0,0,0,0.4)]">Will Update Soon</h2>
-      </div>
-  );
-}
